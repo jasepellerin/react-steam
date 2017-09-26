0077f0e1fcf3b966b6c86be5c93918ea0fb16c1d
Jase Pellerin <jasepellerin@gmail.com>
2017-09-26 11:53:53 -0600

    Changed steam api endpoint

--- server/steam-api.js
+++ server/steam-api.js
@@ -1,11 +1,19 @@
 import fetch from 'node-fetch'
 const apiKey = require('./steam-api-key.json').key
 
-const testURL = 'https://api.steampowered.com/ISteamUser/GetPlayerSummaries/' +
-  'v2/?key=' + apiKey + '&steamids='
+const ownedGameURL = 'https://api.steampowered.com/IPlayerService/GetOwnedGames/' +
+  'v1/?key=' + apiKey + '&input_json='
+
+let inputJson = {
+  'steamid': 0,
+  'include_appinfo': true,
+  'include_played_free_games': false,
+  'appids_filter': false
+}
 
 function getApiResults(query) {
-  return fetch(testURL + query)
+  inputJson.steamid = query
+  return fetch(ownedGameURL + JSON.stringify(inputJson))
     .then(function (response) {
       if (response.ok) {
         return response.json()
@@ -13,11 +21,7 @@ function getApiResults(query) {
         throw new Error('Response not ok')
       }
     }).then(response => {
-      const players = response.response.players
-      if (players.length > 0) {
-        return players
-      }
-      return { 'redirect': 'static/html/' }
+      return response
     })
     .catch(function (error) {
       return error
