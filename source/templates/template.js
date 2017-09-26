dbdd3d338acb68edad633824c427e03ad0387eaa
Jase Pellerin <jasepellerin@gmail.com>
2017-09-26 13:44:48 -0600

    Remove warning comment

--- source/templates/template.js
+++ source/templates/template.js
@@ -13,8 +13,6 @@ module.exports = ({ title, body, preloadedState }) => {
       <body>
         <div id="root">${body}</div>
         <script>
-        // WARNING: See the following for security issues around embedding JSON in HTML:
-        // http://redux.js.org/docs/recipes/ServerRendering.html#security-considerations
         window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}
       </script>
       </body>
