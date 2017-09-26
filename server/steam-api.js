97c7e44a7223b38393fb37f2098bedb2d5877eda
Jase Pellerin <jasepellerin@gmail.com>
2017-09-26 11:40:10 -0600

    Changed api import

--- server/steam-api.js
+++ server/steam-api.js
@@ -1,4 +1,4 @@
-const fetch = require('node-fetch')
+import fetch from 'node-fetch'
 const apiKey = require('./steam-api-key.json').key
 
 const testURL = 'https://api.steampowered.com/ISteamUser/GetPlayerSummaries/' +
