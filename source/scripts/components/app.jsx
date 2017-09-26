57f231ec805b9863a675f3ed764a3754dbfc2649
Jase Pellerin <jasepellerin@gmail.com>
2017-09-26 14:04:57 -0600

    Added starter profile component

--- source/scripts/components/app.jsx
+++ source/scripts/components/app.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import SearchForm from './search_form.jsx'
+import Profile from './profile.jsx'
 
 export class App extends React.Component {
   render() {
     return (
       <div className="centered full-centered">
         <SearchForm />
+        <Profile />
       </div>
     )
   }
---
+++ source/scripts/components/profile.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Profile = ({ dispatch }) => {
+  return (
+    <div>Hi</div>
+  )
+}
+
+export default Profile
