91bec6c24fb76cb75b9eb493c83aa18da611f37e
Jase Pellerin <jasepellerin@gmail.com>
2017-09-27 13:38:13 -0600

    Sorted games by playtime

--- source/scripts/redux/reducers/games.js
+++ source/scripts/redux/reducers/games.js
@@ -2,10 +2,13 @@ export default (state = [], action) => {
   switch (action.type) {
     case 'UPDATE_GAMES':
       let array = []
+      // Add an ID to each game
       for (let id = 0; id < action.array.length; id++) {
         array[id] = action.array[id]
         array[id].id = id
       }
+      // Sort games based on descending playtime
+      array.sort((a, b) => b.playtime_forever - a.playtime_forever)
       return array
     default:
       return state
