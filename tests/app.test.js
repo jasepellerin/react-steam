5aca1462a49f56f07bcabd68ec45e50a856ecd41
Jase Pellerin <jasepellerin@gmail.com>
2017-09-28 14:33:22 -0600

    Fixed test

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { App } from '../source/scripts/components/app.jsx'
+import { App } from '../source/scripts/components/App.jsx'
 
 test('App works with React syntax', () => {
   expect(<App />).toBeDefined()
