1593d19117031d90e08fda1ec6dbb7d72d9b52c9
Jase Pellerin <jasepellerin@gmail.com>
2017-09-28 11:25:19 -0600

    Refactored SearchForm

--- source/scripts/components/SearchForm.jsx
+++ source/scripts/components/SearchForm.jsx
@@ -1,29 +1,31 @@
 import React from 'react'
 
 class SearchForm extends React.Component {
+  onSubmit(e, input) {
+    e.preventDefault()
+    const query = input.value
+    if (query.length === 0) {
+      return
+    }
+    fetch('/search/' + query).then(result => result.json())
+      .then(result => {
+        if (result.hasError) {
+          this.props.updateQuery(query)
+          this.props.updateGames()
+        } else {
+          this.props.updateQuery(query)
+          this.props.updateGames(result.games)
+        }
+      })
+  }
+
   render() {
     let input
 
     return (
       <form
         className='search'
-        onSubmit={e => {
-          e.preventDefault()
-          const query = input.value
-          if (query.length === 0) {
-            return
-          }
-          fetch('/search/' + query).then(result => result.json())
-            .then(result => {
-              if (result.hasError) {
-                this.props.updateQuery(query)
-                this.props.updateGames()
-              } else {
-                this.props.updateQuery(query)
-                this.props.updateGames(result.games)
-              }
-            })
-        }}>
+        onSubmit={(e) => this.onSubmit(e, input)}>
         <input defaultValue={this.props.query} ref={node => {
           input = node
         }}
