88818cb73355db5b202960197c5a2ecdac886832
Jase Pellerin <jasepellerin@gmail.com>
2017-09-28 09:39:05 -0600

    Refactored search form into class

--- source/scripts/components/search-form.jsx
+++ source/scripts/components/search-form.jsx
@@ -1,38 +1,45 @@
 import React from 'react'
+import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { query, games } from '../redux/actions'
 
-let SearchForm = ({ dispatch }) => {
-  let input
-
-  return (
-    <form
-      className='search'
-      onSubmit={e => {
-        e.preventDefault()
-        if (!input.value.trim()) {
-          return
-        }
-        fetch('/search/' + input.value).then(result => result.json())
-          .then(result => {
-            if (typeof result === 'string') {
-              dispatch(games([]))
-            } else {
-              dispatch(games(result.games))
-            }
-          })
-        dispatch(query(input.value))
-      }}>
-      <input ref={node => {
-        input = node
-      }}
-        type='search'
-        placeholder='Find users...' />
-      <button
-        className='button'
-        type='submit'>Search</button>
-    </form>
-  )
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ games, query }, dispatch)
 }
 
-export default connect()(SearchForm)
+class SearchForm extends React.Component {
+  render() {
+    let input
+
+    return (
+      <form
+        className='search'
+        onSubmit={e => {
+          e.preventDefault()
+          if (!input.value.trim()) {
+            return
+          }
+          fetch('/search/' + input.value).then(result => result.json())
+            .then(result => {
+              if (typeof result === 'string') {
+                this.props.games([])
+              } else {
+                this.props.games(result.games)
+              }
+            })
+          this.props.query(input.value)
+        }}>
+        <input ref={node => {
+          input = node
+        }}
+          type='search'
+          placeholder='Find users...' />
+        <button
+          className='button'
+          type='submit'>Search</button>
+      </form>
+    )
+  }
+}
+
+export default connect(null, mapDispatchToProps)(SearchForm)
