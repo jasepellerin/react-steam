42c776386ef50326d9ec0f5d5f089e05a21366e0
Jase Pellerin <jasepellerin@gmail.com>
2017-09-28 11:22:56 -0600

    Refactored TopBar

--- source/scripts/components/TopBar.jsx
+++ source/scripts/components/TopBar.jsx
@@ -5,26 +5,32 @@ const Title = () => (<h1>Steam User Information</h1>)
 
 // If games exist, wrap title and search in top bar
 class TopBar extends React.Component {
+  getTopBar(gamesExist) {
+    if (gamesExist) {
+      return (
+        <div className='top-bar'>
+          <div className='top-bar-left'>
+            <Title />
+          </div>
+          <div className='top-bar-right'>
+            <SearchFormContainer />
+          </div>
+        </div>
+      )
+    } else {
+      return (
+        <div className='full-height'>
+          <Title />
+          < SearchFormContainer />
+        </div>
+      )
+    }
+  }
+
   render() {
     let games = this.props.games
     let gamesExist = games && games.length > 0
-    return (
-      <div className={gamesExist ? 'top-bar' : 'full-height'}>
-        {gamesExist
-          // Return array of elements, as they don't need a base element
-          ? [
-            <div key={1} className='top-bar-left'>
-              <Title />
-            </div>,
-            <div key={2} className='top-bar-right'>
-              <SearchFormContainer />
-            </div>]
-          : [
-            <Title key={1} />,
-            < SearchFormContainer key={2} />]
-        }
-      </div>
-    )
+    return this.getTopBar(gamesExist)
   }
 }
 
