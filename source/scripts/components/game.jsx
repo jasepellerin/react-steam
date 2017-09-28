f03ce578f3e84ad90ee18697f191aabc83398f47
Jase Pellerin <jasepellerin@gmail.com>
2017-09-28 09:41:01 -0600

    Refactored game into class

--- source/scripts/components/game.jsx
+++ source/scripts/components/game.jsx
@@ -6,26 +6,29 @@ const baseImgURL = 'http://media.steampowered.com/steamcommunity/' +
   'public/images/apps/'
 const imgExt = '.jpg'
 
-const Game = ({ game }) => (
-  <div className='card game'>
-    <a
-      href={baseURL + game.appid}
-      target='_blank'>
-      <div className='card-divider'>
-        <h3>{game.name}</h3>
+class Game extends React.Component {
+  render() {
+    const game = this.props.game
+    return <div className='card game'>
+      <a
+        href={baseURL + game.appid}
+        target='_blank'>
+        <div className='card-divider'>
+          <h3>{game.name}</h3>
+        </div>
+      </a>
+      {game.img_logo_url
+        ? <img
+          alt={game.name}
+          title={game.name}
+          src={baseImgURL + game.appid + '/' + game.img_logo_url + imgExt} />
+        : 'No Image'
+      }
+      <div className="card-section">
+        <p>Playtime: {writeDuration(game.playtime_forever)}</p>
       </div>
-    </a>
-    {game.img_logo_url
-      ? <img
-        alt={game.name}
-        title={game.name}
-        src={baseImgURL + game.appid + '/' + game.img_logo_url + imgExt} />
-      : 'No Image'
-    }
-    <div className="card-section">
-      <p>Playtime: {writeDuration(game.playtime_forever)}</p>
     </div>
-  </div>
-)
+  }
+}
 
 export default Game
